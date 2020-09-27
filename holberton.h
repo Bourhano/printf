de2d4a9a3e1aa3449746b087aef64f235e3dd73a
Mohamad Abdallah <mohamad1031998@gmail.com>
2020-09-27 16:44:24 +0200

    update .h

---
+++ holberton.h
@@ -0,0 +1,12 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+int _printf(const char *format, ...);
+
+#endif /*_HOLBERTON_H_*/
